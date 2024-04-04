f95437b8c63df41fbced15babc13aad13e0f8487
yukiharada1228 <yukiharada1228@gmail.com>
2024-04-04 23:50:33 +0000

    add

--- bitnetb158/replace_hf.py
+++ bitnetb158/replace_hf.py
@@ -55,3 +55,27 @@ def replace_layers_with_bitb158_layers(model):
             )
         else:
             replace_layers_with_bitb158_layers(module)
+
+
+def replace_linear_with_bitlinearb158(model):
+    for name, module in model.named_children():
+        if isinstance(module, nn.Linear):
+            in_features = module.in_features
+            out_features = module.out_features
+            bias = module.bias is not None
+            weight_device = module.weight.device
+            weight_dtype = module.weight.dtype
+
+            setattr(
+                model,
+                name,
+                BitLinearb158(
+                    in_features=in_features,
+                    out_features=out_features,
+                    bias=bias,
+                    device=weight_device,
+                    dtype=weight_dtype,
+                ),
+            )
+        else:
+            replace_linear_with_bitlinearb158(module)
