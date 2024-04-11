56cf9b4e4a3645720bb2d0977308e03916dbd8f7
yukiharada1228 <yukiharada1228@gmail.com>
2024-04-11 12:03:14 +0000

    fix: cfg

--- bitnetb158/models/bit_deit_b158.py
+++ bitnetb158/models/bit_deit_b158.py
@@ -1,7 +1,7 @@
 from functools import partial
 
 import torch.nn as nn
-from timm.models.vision_transformer import VisionTransformer, _cfg
+from timm.models.vision_transformer import VisionTransformer
 
 from ..replace_hf import replace_linear_with_bitlinearb158
 
@@ -17,6 +17,5 @@ def bit_deit_tiny_patch16_224_b158(num_classes=1000):
         qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
     )
-    model.default_cfg = _cfg()
     replace_linear_with_bitlinearb158(model)
     return model
